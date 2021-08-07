154f140d402c4fe26a9d33e5714e70efaa611d53
Fina Silva-Santisteban <fina.santiste@gmail.com>
2021-08-07 12:20:46 -0700

    add boto3 to pipfile

--- aws_resources.py
+++ aws_resources.py
@@ -1,11 +1,28 @@
 import configparser
+import boto3
+
+aws_users = {'admin': 'AWS_CREDS_ADMIN',
+             'redshift': 'AWS_CREDS_REDSHIFT'}
 
 
-def load_credentials():
+def load_credentials(aws_user):
+    """
+    Loads aws credentials for given user.
+    :param aws_user: aws user
+    :return: two strings
+    """
     config = configparser.ConfigParser()
     config.read_file((open(r'dwh.cfg')))
-    path1 = config.get('CLUSTER', 'HOST')
+
+    aws_access_key_id = config.get(aws_users[aws_user], 'AWS_ACCESS_KEY_ID')
+    aws_secret_access_key_id = config.get(aws_users[aws_user], 'AWS_SECRET_ACCESS_KEY_ID')
+
+    return aws_access_key_id, aws_secret_access_key_id
+
+
+def create_s3_bucket():
+    pass
 
 
 if __name__ == "__main__":
-    load_credentials()
+    load_credentials('admin')
