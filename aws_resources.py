3e0ae70e732815eb9b41648056f931e4a35092bc
Fina Silva-Santisteban <fina.santiste@gmail.com>
2021-08-07 12:36:38 -0700

    add method to create bucket

--- aws_resources.py
+++ aws_resources.py
@@ -5,24 +5,47 @@ aws_users = {'admin': 'AWS_CREDS_ADMIN',
              'redshift': 'AWS_CREDS_REDSHIFT'}
 
 
-def load_credentials(aws_user):
+def get_aws_credentials(aws_user):
     """
-    Loads aws credentials for given user.
+    Returns aws credentials for given user.
     :param aws_user: aws user
-    :return: two strings
+    :return: three strings
     """
     config = configparser.ConfigParser()
     config.read_file((open(r'dwh.cfg')))
 
     aws_access_key_id = config.get(aws_users[aws_user], 'AWS_ACCESS_KEY_ID')
-    aws_secret_access_key_id = config.get(aws_users[aws_user], 'AWS_SECRET_ACCESS_KEY_ID')
+    aws_secret_access_key = config.get(aws_users[aws_user], 'AWS_SECRET_ACCESS_KEY')
+    aws_region = config.get(aws_users[aws_user], 'AWS_REGION')
 
-    return aws_access_key_id, aws_secret_access_key_id
+    return aws_access_key_id, aws_secret_access_key, aws_region
+
+
+def get_boto3_session(aws_user):
+    """
+    Returns authenticated boto3 session.
+    :param aws_user: aws user to create session for
+    :return:
+    """
+    aws_access_key_id, aws_secret_access_key, aws_region = get_aws_credentials(aws_user)
+
+    session = boto3.Session(aws_access_key_id=aws_access_key_id,
+                            aws_secret_access_key=aws_secret_access_key,
+                            region_name=aws_region)
+
+    print(session)
+
+    return session
 
 
 def create_s3_bucket():
-    pass
+    aws_user = 'admin'
+    session = get_boto3_session(aws_user)
+    s3_client = session.client('s3')
 
+    s3_client.create_bucket(Bucket='finas-test-bucket')
 
 if __name__ == "__main__":
-    load_credentials('admin')
+    result = create_s3_bucket()
+
+    print(result)
