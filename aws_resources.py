807d693f9c12a6c3f5ced15b3df0a906e4d18970
Fina Silva-Santisteban <fina.santiste@gmail.com>
2021-08-14 12:25:23 -0700

    cleanup

--- aws_resources.py
+++ aws_resources.py
@@ -1,33 +1,15 @@
-from aws.AWSClient import AWSClient
 from aws.RedshiftCluster import RedshiftCluster
 from db.DBHandler import DBHandler
-
-def create_s3_bucket():
-    """
-    Creates an s3 bucket called 'sparkify-fina'.
-    :return:
-    """
-    s3_client = AWSClient(resource='s3').client
-    s3_client.create_bucket(Bucket='sparkify-fina')
-
-
-def delete_s3_bucket():
-    """
-    Deletes s3 bucket called 'sparkify-fina'.
-    :return:
-    """
-    s3_client = AWSClient(resource='s3').client
-    s3_client.delete_bucket(Bucket='sparkify-fina')
 
 
 if __name__ == "__main__":
     rc = RedshiftCluster()
 
-    rc.describe_cluster()
+    # rc.describe_cluster()
 
     # rc.create_all_resources()
-    # rc.delete_all_resources()
+    rc.delete_all_resources()
 
-    db = DBHandler(redshiftCluster=rc)
+    # db = DBHandler(redshiftCluster=rc)
 
-    db.get_db_connection()
+    # db.get_db_connection()
