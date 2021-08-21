e927a513a6b4d11d21d318fc4f3785d045c0730c
Fina Silva-Santisteban <fina.santiste@gmail.com>
2021-08-21 15:41:49 -0700

    limit song data set for coyping during local work

--- sql_queries.py
+++ sql_queries.py
@@ -111,7 +111,7 @@ staging_events_copy = ("""
             s3_staging_data_region)
 
 staging_songs_copy = ("""
-    copy staging_songs from '{}/A'
+    copy staging_songs from '{}/A/A/A'
     credentials 'aws_iam_role={}'
     json 'auto'
     compupdate off region '{}';
@@ -144,6 +144,6 @@ create_table_queries = [staging_events_table_create,
 # drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 drop_table_queries = [staging_events_table_drop,
                       staging_songs_table_drop]
-# copy_table_queries = [staging_events_copy, staging_songs_copy]
-copy_table_queries = [staging_songs_copy]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+# copy_table_queries = [staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
