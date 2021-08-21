0300dbb3b479ba8b758c7483fc8a02c64e89107b
Fina Silva-Santisteban <fina.santiste@gmail.com>
2021-08-21 15:23:50 -0700

    copy events data into events staging table

--- sql_queries.py
+++ sql_queries.py
@@ -31,10 +31,10 @@ s3_staging_data_region = 'us-west-2'
 # DROP TABLES
 
 staging_events_table_drop = """
-DROP TABLE IF EXISTS "staging_events_table";
+DROP TABLE IF EXISTS "staging_events";
 """
 staging_songs_table_drop = """
-DROP TABLE IF EXISTS "staging_songs_table";
+DROP TABLE IF EXISTS "staging_songs";
 """
 songplay_table_drop = ""
 user_table_drop = ""
@@ -46,41 +46,39 @@ time_table_drop = ""
 
 staging_events_table_create = ("""
 CREATE TABLE "staging_events" (
-    "id" double precision DEFAULT nextval('staging_events_table_seq') NOT NULL,
     "artist" VARCHAR(250),
-    "auth" VARCHAR(250) NOT NULL,
-    "first_name" VARCHAR(250) NOT NULL,
-    "gender" VARCHAR(250) NOT NULL,
-    "item_in_session" NUMERIC NOT NULL,
-    "last_name" VARCHAR(250) NOT NULL,
+    "auth" VARCHAR(250),
+    "first_name" VARCHAR(250),
+    "gender" VARCHAR(250),
+    "item_in_session" NUMERIC,
+    "last_name" VARCHAR(250),
     "length" NUMERIC,
-    "level" VARCHAR(250) NOT NULL,
-    "location" VARCHAR(250) NOT NULL,
-    "method" VARCHAR(250) NOT NULL,
-    "page" VARCHAR(250) NOT NULL,
-    "registration" NUMERIC NOT NULL,
-    "session_id" NUMERIC NOT NULL,
+    "level" VARCHAR(250),
+    "location" VARCHAR(250),
+    "method" VARCHAR(250),
+    "page" VARCHAR(250),
+    "registration" NUMERIC,
+    "session_id" NUMERIC,
     "song" VARCHAR(250),
-    "status" NUMERIC NOT NULL,
+    "status" NUMERIC,
     "ts" NUMERIC NOT NULL,
-    "user_agent" VARCHAR(250) NOT NULL,
-    "user_id" NUMERIC NOT NULL
+    "user_agent" VARCHAR(250),
+    "user_id" NUMERIC
 );
 """)
 
 staging_songs_table_create = ("""
 CREATE TABLE "staging_songs" (
-    "id" double precision DEFAULT nextval('staging_songs_table_seq') NOT NULL,
-    "num_songs" NUMERIC NOT NULL,
+    "num_songs" NUMERIC,
     "artist_id" VARCHAR(250) NOT NULL,
     "artist_latitude" VARCHAR(250),
     "artist_longitude" VARCHAR(250),
     "artist_location" VARCHAR(250),
-    "artist_name" VARCHAR(250) NOT NULL,
+    "artist_name" VARCHAR(250),
     "song_id" VARCHAR(250) NOT NULL,
-    "title" VARCHAR(250) NOT NULL,
-    "duration" NUMERIC NOT NULL,
-    "year" NUMERIC NOT NULL
+    "title" VARCHAR(250),
+    "duration" NUMERIC,
+    "year" NUMERIC
 );
 """)
 
@@ -102,9 +100,10 @@ time_table_create = ("""
 # STAGING TABLES
 
 staging_events_copy = ("""
-    copy staging_events from '{}'
+    copy staging_events from '{}/2018/11'
     credentials 'aws_iam_role={}'
-    gzip delimiter ';' compupdate off region '{}';
+    json 'auto'
+    compupdate off region '{}';
 """).format(config.get('S3', 'LOG_DATA'),
             iam_role_arn,
             s3_staging_data_region)
