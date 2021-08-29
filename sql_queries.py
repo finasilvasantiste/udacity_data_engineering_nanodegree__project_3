a7b6fd94712f7aa5f589f6e4ccf8edf632e3e4c5
Fina Silva-Santisteban <fina.santiste@gmail.com>
2021-08-29 10:02:23 -0700

    update songplay insert query

--- sql_queries.py
+++ sql_queries.py
@@ -94,15 +94,15 @@ CREATE TABLE "staging_songs" (
 );
 """)
 
+
 songplay_table_create = ("""
-CREATE TABLE "songplay_fact" (
-    "songplay_id" double precision DEFAULT nextval('songplay_fact_seq') NOT NULL,
-    "start_time" TIMESTAMP NOT NULL,
+CREATE TABLE "songplays_fact" (
+    "songplay_id" INT IDENTITY(0,1) PRIMARY KEY,
+    "start_time" TIMESTAMP,
     "user_id" VARCHAR(250),
     "level" VARCHAR(250),
     "song_id" VARCHAR(250),
     "artist_id" VARCHAR(250),
-    "song_id" VARCHAR(250) NOT NULL,
     "session_id" VARCHAR(250),
     "location" VARCHAR(250),
     "user_agent" VARCHAR(250)
@@ -174,9 +174,16 @@ staging_songs_copy = ("""
 # FINAL TABLES
 
 songplay_table_insert = ("""
-INSERT INTO songplay_fact (start_time)
-SELECT start_time
-FROM times_dim;
+INSERT INTO songplays_fact (start_time, user_id, 
+level, song_id, artist_id, session_id, location, user_agent)
+SELECT TIMESTAMP 'epoch' + e.ts/1000 * interval '1 second' AS start_time,
+e.user_id, e.level, s.song_id, s.artist_id, e.session_id,
+e.location, e.user_agent
+FROM staging_events e JOIN staging_songs s 
+ON e.artist=s.artist_name
+AND e.song=s.title
+AND e.length=s.duration
+WHERE e.artist IS NOT NULL;
 
 """)
 
@@ -216,18 +223,20 @@ FROM times_dim;
 
 # create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 # create_table_queries = [staging_events_table_create, staging_songs_table_create]
-# create_table_queries = [songplay_table_create]
-create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [songplay_table_create]
+# create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create]
+
 # drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 # drop_table_queries = [staging_events_table_drop, staging_songs_table_drop]
-drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-# drop_table_queries = [songplay_table_drop]
+# drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+drop_table_queries = [songplay_table_drop]
 
 copy_table_queries = [staging_events_copy,
                       staging_songs_copy]
+
 # insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
-insert_table_queries = [user_table_insert,
-                        song_table_insert,
-                        artist_table_insert,
-                        time_table_insert]
-# insert_table_queries = [songplay_table_insert]
+# insert_table_queries = [user_table_insert,
+#                         song_table_insert,
+#                         artist_table_insert,
+#                         time_table_insert]
+insert_table_queries = [songplay_table_insert]
